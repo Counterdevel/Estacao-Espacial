0340da43257fb2a5badaf49250878ee59d230b49
Pierre Muller <pierreeitel@hotmail.com>
2021-04-20 21:02:11 -0300

    Atividade Estação Espacial Concluida

---
+++ NavMeshStationStarter/Assets/AIControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public NavMeshAgent agent; //Variavel do tipo NAvMeshAgent
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>(); //Pega o componente de NAvMeshAgent
+    }
+
+}
---
+++ NavMeshStationStarter/Assets/AgentManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents; //Array de gameobjects
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai"); //Encontra gameobjects com a tag "ai" na scene
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) //Condicional para o click do mouse
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100)) //Caso o raycast atinja um local em uma distancia de 100, o agent se move até o local
+            {
+                foreach (GameObject a in agents)
+                {
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point); //Pega o component da classe AIControl
+                }
+            }
+        }
+    }
+}
